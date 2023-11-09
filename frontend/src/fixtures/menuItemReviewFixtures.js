b2bd74ca774e28a4979cbedf347a72b785cb257c
Richard Huang <91812464+huangderful@users.noreply.github.com>
2023-11-08 17:17:03 -0800

    changed fixtures to contain correct attribute names

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -3,7 +3,8 @@ const menuItemReviewFixtures = {
         "id": 1,
         "itemId": 33,
         "stars": 5,
-        "localDateTime": "2022-01-02T12:00:00",
+        "dateReviewed": "2022-01-02T12:00:00",
+        "reviewerEmail": "richard.@gmail.com",
         "comments": "This stinks"
     },
     menuItemReview2: [
@@ -11,21 +12,24 @@ const menuItemReviewFixtures = {
             "id": 1,
             "itemId": 50,
             "stars": -1,
-            "localDateTime": "2023-01-02T12:00:00",
+            "dateReviewed": "2023-01-02T12:00:00",
+            "reviewerEmail": "richard.@gmail.com",
             "comments": "This overly"
         },
         {
             "id": 2,
             "itemId": 5,
             "stars": 5,
-            "localDateTime": "2022-04-03T12:00:00",
+            "dateReviewed": "2022-04-03T12:00:00",
+            "reviewerEmail": "richard.@gmail.com",
             "comments": "This stinks"
         },
         {
             "id": 3,
             "itemId": 20,
             "stars": 3,
-            "localDateTime": "2022-07-04T12:00:00",
+            "dateReviewed": "2022-07-04T12:00:00",
+            "reviewerEmail": "richard.@gmail.com",
             "comments": "This massively stink s"
         }
     ],
@@ -34,7 +38,8 @@ const menuItemReviewFixtures = {
             "id": 1,
             "itemId": 33,
             "stars": 3,
-            "localDateTime": "2022-01-02T12:00:00",
+            "dateReviewed": "2022-01-02T12:00:00",
+            "reviewerEmail": "richard.@gmail.com",
             "comments": "This kinda stinks"
         }
     ]
