84c1b2528e87ed539f4d7efcd43fc4314340d545
tigeryu8900 <tigeryu8900@gmail.com>
2023-11-09 01:56:17 -0800

    ty - Index Page for UCSBDiningCommonsMenuItem plus tests and stories

--- frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemIndexPage.js
+++ frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemIndexPage.js
@@ -1,13 +1,37 @@
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import React from 'react'
+import {useBackend} from 'main/utils/useBackend';
+
+import BasicLayout from 'main/layouts/BasicLayout/BasicLayout';
+import UCSBDiningCommonsMenuItemTable from 'main/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemTable';
+import {hasRole, useCurrentUser} from 'main/utils/currentUser'
+import {Button} from 'react-bootstrap';
 
 export default function UCSBDiningCommonsMenuItemIndexPage() {
 
-    // Stryker disable all : placeholder for future implementation
+    const currentUser = useCurrentUser();
+
+    const createButton = () => {
+        if (hasRole(currentUser, "ROLE_ADMIN")) {
+            return (<Button
+                variant="primary"
+                href="/ucsbdiningcommonsmenuitem/create"
+                style={{float: "right"}}
+            >
+                Create UCSB Dining Commons Menu Item
+            </Button>)
+        }
+    }
+
+    const {
+        data: items, error: _error, status: _status
+    } = useBackend(// Stryker disable next-line all : don't test internal caching of React Query
+        ["/api/ucsbdiningcommonsmenuitem/all"], {method: "GET", url: "/api/ucsbdiningcommonsmenuitem/all"}, []);
+
     return (<BasicLayout>
             <div className="pt-2">
-                <h1>Index page not yet implemented</h1>
-                <p><a href="/placeholder/create">Create</a></p>
-                <p><a href="/placeholder/edit/1">Edit</a></p>
+                {createButton()}
+                <h1>UCSB Dining Commons Menu Item</h1>
+                <UCSBDiningCommonsMenuItemTable menuItem={items} currentUser={currentUser}/>
             </div>
-        </BasicLayout>)
+    </BasicLayout>);
 }
